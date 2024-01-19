03e588fdb935166c4c84537e361a5cf71a7a035f
DoroDore <tlyz82@student.cis.edu.hk>
2024-01-19 11:30:42 +0800

    Speedrunning

---
+++ src/Author.java
@@ -0,0 +1,27 @@
+public class Author {
+    private String mName;
+    private String mEmail;
+    private char mGender;
+    public Author(String name, String email, char gender) {
+        mName = name;
+        mEmail = email;
+        mGender = gender;
+    }
+    public void setEmail(String email) {
+        mEmail = email;
+    }
+    public String getEmail() {
+        return mEmail;
+    }
+    public String getName() {
+        return mName;
+    }
+    public char getGender() {
+        return mGender;
+    }
+    @Override
+    public String toString() {
+        return (mName + " (" + mGender + ") at " + mEmail);
+    }
+
+}
---
+++ src/Book.java
@@ -0,0 +1,34 @@
+public class Book {
+    private String mName;
+    private Author mAuthor;
+    private int mPrice;
+    private int mQuantity;
+    public Book(String name, Author author, int price, int quantity) {
+        mName = name;
+        mAuthor = author;
+        mPrice = price;
+        mQuantity = quantity;
+    }
+    public String getName() {
+        return mName;
+    }
+    public String getAuthor() {
+        return (mAuthor.getName() + " (" + mAuthor.getGender() + ") at " + mAuthor.getEmail());
+    }
+    public int getPrice() {
+        return mPrice;
+    }
+    public void setPrice(int price) {
+        mPrice = price;
+    }
+    public int getQty() {
+        return mQuantity;
+    }
+    public void setQty(int quantity) {
+        mQuantity = quantity;
+    }
+    @Override
+    public String toString() {
+        return ("\'" + mName + "\' by " + getAuthor());
+    }
+}
